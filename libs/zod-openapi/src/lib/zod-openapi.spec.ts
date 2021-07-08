d7d31ce069f992ce899214088b890ace8511699b
Brian McBride <Brian-McBride@users.noreply.github.com>
2021-07-08 14:52:57 -0700

    Spec generator for zod types

--- libs/zod-openapi/src/lib/zod-openapi.spec.ts
+++ libs/zod-openapi/src/lib/zod-openapi.spec.ts
@@ -1,45 +1,338 @@
 import { inspect } from 'util';
-import { z, ZodTypeAny } from 'zod';
+import { z, ZodString, ZodTypeAny } from 'zod';
 import {
   OpenApiBuilder,
   OperationObject,
   SchemaObject,
   ReferenceObject,
+  isSchemaObject,
 } from 'openapi3-ts';
+import validator from 'validator';
 
 import { openApi, generateSchema } from './zod-openapi';
 
 describe('zodOpenapi', () => {
-  const description = `The description field of a schema`;
-
-  const zodSchema = z.object({
-    name: openApi(z.string(), { description: `User full name` }),
-    email: openApi(z.string().email(), { description: 'User email' }),
+  /**
+   * Primitives
+   */
+  it('should support basic primitives', () => {
+    const zodSchema = openApi(
+      z.object({
+        aString: z.string().optional(),
+        aNumber: z.number().optional(),
+        aBigInt: z.bigint(),
+        aBoolean: z.boolean(),
+        aDate: z.date(),
+      }),
+      {
+        description: `Primitives also testing overwriting of "required"`,
+        required: ['aNumber'], // All schema settings "merge"
+      }
+    );
+    const apiSchema = generateSchema(zodSchema);
+    expect(apiSchema).toEqual({
+      type: 'object',
+      properties: {
+        aString: { type: 'string' },
+        aNumber: { type: 'number' },
+        aBigInt: { type: 'integer', format: 'int64' },
+        aBoolean: { type: 'boolean' },
+        aDate: { type: 'string', format: 'date-time' },
+      },
+      required: ['aBigInt', 'aBoolean', 'aDate', 'aNumber'],
+      description: 'Primitives also testing overwriting of "required"',
+    });
   });
 
-  // it('It should handle a string type', () => {
-  //   const zodTest = openApi(z.string(), {
-  //     description,
-  //   });
-  //   const schemaTest = generateSchema(zodTest);
-  //   expect(schemaTest.description).toEqual(description);
-  // });
+  it('should support empty types', () => {
+    const zodSchema = openApi(
+      z.object({
+        aUndefined: z.undefined(),
+        aNull: z.null(),
+        aVoid: z.void(),
+      }),
+      {
+        description: `Empty types in a schema`,
+      }
+    );
+    const apiSchema = generateSchema(zodSchema);
+    expect(apiSchema).toEqual({
+      type: 'object',
+      properties: {
+        aUndefined: {},
+        aNull: { nullable: true, type: 'string', format: 'null' },
+        aVoid: { nullable: true },
+      },
+      required: ['aNull'],
+      description: 'Empty types in a schema',
+    });
+  });
 
-  it('It should handle a object with string type', () => {
-    const schemaTest = generateSchema(zodSchema);
-    expect(
-      schemaTest.properties &&
-        'description' in schemaTest.properties?.name &&
-        schemaTest.properties?.name.description
-    ).toEqual('User full name'); // ! Bullshit. This should generate the typed object too
+  it('It should support proper transform input/output', () => {
+    const zodTransform = openApi(
+      z.string().transform((val) => val.length),
+      { description: 'Will take in a string, returning the length' }
+    );
+    const schemaIn = generateSchema(zodTransform);
+    expect(schemaIn.type).toEqual('string');
+    const schemaOut = generateSchema(zodTransform, true);
+    expect(schemaOut.type).toEqual('number');
+  });
 
-    const builder = OpenApiBuilder.create();
-    builder.addSchema('Users', schemaTest);
+  it('should support catch-all types', () => {
+    const zodSchema = openApi(
+      z.object({
+        aAny: z.any(),
+        aUnknown: z.unknown(),
+      }),
+      {
+        description: `Empty types don't belong in schema`,
+      }
+    );
+    const apiSchema = generateSchema(zodSchema);
+    expect(apiSchema.properties).toBeDefined();
+    expect((apiSchema.properties?.aAny as SchemaObject).nullable).toEqual(true);
+    expect((apiSchema.properties?.aUnknown as SchemaObject).nullable).toEqual(
+      true
+    );
+  });
 
-    builder; //?
+  it('should support never type', () => {
+    const zodSchema = openApi(
+      z.object({
+        aNever: z.never(),
+      }),
+      {
+        description: `Never values! Can not use them!`,
+      }
+    );
+    const apiSchema = generateSchema(zodSchema);
+    expect(apiSchema.properties).toBeDefined();
+    expect((apiSchema.properties?.aNever as SchemaObject).readOnly).toEqual(
+      true
+    );
+  });
 
-    console.log(schemaTest);
-    // console.log(zodTest.shape);
-    console.log(builder);
+  it('should support string and string constraints', () => {
+    const zodSchema = openApi(
+      z
+        .object({
+          aStringMax: z.string().max(5),
+          aStringMin: z.string().min(3),
+          aStringLength: z.string().length(10),
+          aStringEmail: z.string().email(),
+          aStringUrl: z.string().url(),
+          aStringUUID: z.string().uuid(),
+          aStringRegex: z.string().regex(/^[a-zA-Z]+$/),
+          aStringNonEmpty: z.string().nonempty(),
+        })
+        .partial(),
+      {
+        description: `This is totally unlike String Theory`,
+      }
+    );
+    const apiSchema = generateSchema(zodSchema);
+    expect(apiSchema).toEqual({
+      type: 'object',
+      properties: {
+        aStringMax: { type: 'string', maxLength: 5 },
+        aStringMin: { type: 'string', minLength: 3 },
+        aStringLength: { type: 'string', minLength: 10, maxLength: 10 },
+        aStringEmail: { type: 'string', format: 'email' },
+        aStringUrl: { type: 'string', format: 'uri' },
+        aStringUUID: { type: 'string', format: 'uuid' },
+        aStringRegex: { type: 'string', regex: /^[a-zA-Z]+$/ },
+        aStringNonEmpty: { type: 'string', minLength: 1 },
+      },
+      required: [],
+      description: 'This is totally unlike String Theory',
+    });
+  });
+
+  it('should support numbers and number constraints', () => {
+    const zodSchema = openApi(
+      z
+        .object({
+          aNumberMin: z.number().min(3),
+          aNumberMax: z.number().max(8),
+          aNumberInt: z.number().int(),
+          aNumberPositive: z.number().positive(),
+          aNumberNonnegative: z.number().nonnegative(),
+          aNumberNegative: z.number().negative(),
+          aNumberNonpositive: z.number().nonpositive(),
+        })
+        .partial(),
+      {
+        description: `Look mah, the horse can count higher than me!`,
+      }
+    );
+    const apiSchema = generateSchema(zodSchema);
+    expect(apiSchema).toEqual({
+      type: 'object',
+      properties: {
+        aNumberMin: { type: 'number', minimum: 3 },
+        aNumberMax: { type: 'number', maximum: 8 },
+        aNumberInt: { type: 'integer' },
+        aNumberPositive: { type: 'number', minimum: 1 },
+        aNumberNonnegative: { type: 'number', minimum: 0 },
+        aNumberNegative: { type: 'number', maximum: -1 },
+        aNumberNonpositive: { type: 'number', maximum: 0 },
+      },
+      required: [],
+      description: 'Look mah, the horse can count higher than me!',
+    });
+  });
+
+  it('Testing large mixed schema', () => {
+    enum Fruits {
+      Apple,
+      Banana,
+    }
+
+    const MoreFruits = {
+      Pear: 'pear',
+      Plumb: 'plumb',
+      grapes: 3,
+    } as const;
+
+    const zodSchema = z.object({
+      name: openApi(z.string(), { description: `User full name` }),
+      email: openApi(z.string().email().min(4), { description: 'User email' }),
+      whatever: z.string().optional(),
+      myCollection: openApi(
+        z.array(z.object({ name: z.string(), count: z.number() })),
+        { maxItems: 10 }
+      ),
+      timeStamp: z.string().refine(validator.isISO8601),
+      literals: z.object({
+        wordOne: z.literal('One').nullable(),
+        numberTwo: z.literal(2).optional(),
+        isThisTheEnd: z.literal(false).optional().nullable(),
+      }),
+      foodTest: openApi(
+        z.object({
+          fishEnum: openApi(z.enum(['Salmon', 'Tuna', 'Trout']), {
+            description: 'Choose your fish',
+            default: 'Salmon',
+          }),
+          fruitEnum: z.nativeEnum(Fruits),
+          moreFruitsEnum: z.nativeEnum(MoreFruits),
+        }),
+        { description: 'Have some lunch' }
+      ),
+      employedPerson: openApi(
+        z.intersection(
+          z.object({ name: z.string() }),
+          z.object({
+            role: z.enum(['manager', 'employee', 'intern', 'hopeful']),
+          })
+        ),
+        { description: 'Our latest addition' }
+      ),
+      makeAChoice: z.union([z.literal('One'), z.literal(2)]),
+      openChoice: openApi(z.union([z.string(), z.string()]), {
+        description: 'Odd pattern here',
+      }),
+      aNullish: z.string().nullish(),
+      stringLengthOutput: z.string().transform((val) => val.length),
+    });
+
+    const schemaTest = generateSchema(zodSchema, true);
+
+    expect(schemaTest).toEqual({
+      type: 'object',
+      properties: {
+        name: { type: 'string', description: 'User full name' },
+        email: {
+          type: 'string',
+          format: 'email',
+          minLength: 4,
+          description: 'User email',
+        },
+        whatever: { type: 'string' },
+        myCollection: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: { name: { type: 'string' }, count: { type: 'number' } },
+            required: ['name', 'count'],
+          },
+          maxItems: 10,
+        },
+        timeStamp: { type: 'string' },
+        literals: {
+          type: 'object',
+          properties: {
+            wordOne: { nullable: true, type: 'string', enum: ['One'] },
+            numberTwo: { type: 'number', enum: [2] },
+            isThisTheEnd: { nullable: true, type: 'boolean', enum: [false] },
+          },
+          required: ['wordOne'],
+        },
+        foodTest: {
+          type: 'object',
+          properties: {
+            fishEnum: {
+              type: 'string',
+              enum: ['Salmon', 'Tuna', 'Trout'],
+              description: 'Choose your fish',
+              default: 'Salmon',
+            },
+            fruitEnum: { type: 'string', enum: ['Apple', 'Banana', 0, 1] },
+            moreFruitsEnum: { type: 'string', enum: ['pear', 'plumb', 3] },
+          },
+          required: ['fishEnum', 'fruitEnum', 'moreFruitsEnum'],
+          description: 'Have some lunch',
+        },
+        employedPerson: {
+          allOf: [
+            {
+              type: 'object',
+              properties: { name: { type: 'string' } },
+              required: ['name'],
+            },
+            {
+              type: 'object',
+              properties: {
+                role: {
+                  type: 'string',
+                  enum: ['manager', 'employee', 'intern', 'hopeful'],
+                },
+              },
+              required: ['role'],
+            },
+          ],
+          description: 'Our latest addition',
+        },
+        makeAChoice: {
+          oneOf: [
+            { type: 'string', enum: ['One'] },
+            { type: 'number', enum: [2] },
+          ],
+        },
+        openChoice: {
+          oneOf: [{ type: 'string' }, { type: 'string' }],
+          description: 'Odd pattern here',
+        },
+        aNullish: { nullable: true, type: 'string' },
+        stringLengthOutput: { type: 'number' },
+      },
+      required: [
+        'name',
+        'email',
+        'myCollection',
+        'timeStamp',
+        'literals',
+        'foodTest',
+        'employedPerson',
+        'makeAChoice',
+        'openChoice',
+        'stringLengthOutput',
+      ],
+    });
+
+    // const builder = OpenApiBuilder.create();
+    // builder.addSchema('Users', schemaTest);
+    // builder;
   });
 });
--- libs/zod-openapi/src/lib/zod-openapi.ts
+++ libs/zod-openapi/src/lib/zod-openapi.ts
@@ -1,14 +1,15 @@
+// Refactor of  https://github.com/RobinTail/express-zod-api/blob/1fa5991ec12aa461506887ef7de7882d7afe3b48/src/open-api.ts
+
 import merge from 'ts-deepmerge';
 import { z, ZodTypeAny, AnyZodObject } from 'zod';
-import {
-  OpenApiBuilder,
-  OperationObject,
-  SchemaObject,
-  ReferenceObject,
-} from 'openapi3-ts';
+import { SchemaObject } from 'openapi3-ts';
 
-export type OpenApiZodAny = ZodTypeAny & { metaOpenApi?: SchemaObject };
-export type OpenApiZodAnyObject = AnyZodObject & { metaOpenApi?: SchemaObject };
+export interface OpenApiZodAny extends ZodTypeAny {
+  metaOpenApi?: SchemaObject;
+}
+export interface OpenApiZodAnyObject extends AnyZodObject {
+  metaOpenApi?: SchemaObject;
+}
 
 export function openApi<T extends OpenApiZodAny>(
   schema: T,
@@ -18,72 +19,268 @@ export function openApi<T extends OpenApiZodAny>(
   return schema;
 }
 
-const MIME_TYPE_JSON = 'application/json';
+function parseZodTransformation(
+  value: z.ZodTransformer<never> | z.ZodEffects<never>,
+  useOutput = false
+) {
+  const input = parseZodString(value._def.schema);
+  let output = 'undefined';
+  if (useOutput && value._def.effects && value._def.effects.length > 0) {
+    const effect = value._def.effects
+      .filter((ef) => ef.type === 'transform')
+      .slice(-1)[0];
+    if (effect && 'transform' in effect) {
+      try {
+        output = typeof effect.transform(
+          ['integer', 'number'].includes(`${input.type}`)
+            ? 0
+            : 'string' === input.type
+            ? ''
+            : 'boolean' === input.type
+            ? false
+            : 'object' === input.type
+            ? {}
+            : 'null' === input.type
+            ? null
+            : 'array' === input.type
+            ? []
+            : undefined
+        );
+      } catch (e) {
+        /**/
+      }
+    }
+  }
+  return {
+    ...input,
+    ...(['number', 'string', 'boolean', 'null'].includes(output)
+      ? {
+          type: output as 'number' | 'string' | 'boolean' | 'null',
+        }
+      : {}),
+  };
+}
 
 function iterateZodObject(
-  schema: OpenApiZodAnyObject
+  schema: OpenApiZodAnyObject,
+  useOutput = false
 ): Record<string, SchemaObject> {
   return Object.keys(schema.shape).reduce(
     (carry, key) => ({
       ...carry,
-      [key]: generateSchema(schema.shape[key]),
+      [key]: generateSchema(schema.shape[key], useOutput),
     }),
     {} as Record<string, SchemaObject>
   );
 }
 
-export function generateSchema(zodRef: OpenApiZodAny): SchemaObject {
-  const metaSchema = zodRef.metaOpenApi ?? {};
+function parseZodString(item: z.ZodString, metaSchema: SchemaObject = {}) {
+  const baseSchema: SchemaObject = {
+    type: 'string',
+  };
+  if (item.isNullable()) {
+    baseSchema.nullable = true;
+  }
+  const { checks = [] } = item._def;
+  checks.forEach((item) => {
+    switch (item.kind) {
+      case 'email':
+        baseSchema.format = 'email';
+        break;
+      case 'uuid':
+        baseSchema.format = 'uuid';
+        break;
+      case 'url':
+        baseSchema.format = 'uri';
+        break;
+      case 'max':
+        baseSchema.maxLength = item.value;
+        break;
+      case 'min':
+        baseSchema.minLength = item.value;
+        break;
+      case 'regex':
+        baseSchema.regex = item.regex;
+        break;
+    }
+  });
+  return merge(baseSchema, metaSchema);
+}
 
-  // return {
-  //   type: 'object',
-  //   properties: {
-  //     name: {
-  //       type: 'string',
-  //       description: 'User full name',
-  //     },
-  //   },
-  // };
+function parseZodNumber(item: z.ZodNumber, metaSchema: SchemaObject = {}) {
+  const baseSchema: SchemaObject = {
+    type: 'number',
+  };
+  if (item.isNullable()) {
+    baseSchema.nullable = true;
+  }
+  const { checks = [] } = item._def;
+  checks.forEach((item) => {
+    switch (item.kind) {
+      case 'max':
+        baseSchema.maximum = item.value - (item.inclusive ? 0 : 1);
+        break;
+      case 'min':
+        baseSchema.minimum = item.value + (item.inclusive ? 0 : 1);
+        break;
+      case 'int':
+        baseSchema.type = 'integer';
+        break;
+    }
+  });
+  return merge(baseSchema, metaSchema);
+}
+
+export function generateSchema(
+  zodRef: OpenApiZodAny,
+  useOutput = false
+): SchemaObject {
+  const metaSchema = zodRef.metaOpenApi ?? {};
+  const baseSchema: SchemaObject = {};
+
+  if (zodRef.isNullable()) {
+    baseSchema.nullable = true;
+  }
 
   switch (true) {
     case zodRef instanceof z.ZodObject:
+    case zodRef instanceof z.ZodRecord:
       return merge(
         {
           type: 'object',
-          properties: iterateZodObject(zodRef as OpenApiZodAnyObject),
+          properties: iterateZodObject(
+            zodRef as OpenApiZodAnyObject,
+            useOutput
+          ),
+          required: Object.keys((zodRef as z.AnyZodObject).shape).filter(
+            (key) =>
+              !(zodRef as z.AnyZodObject).shape[key].isOptional() &&
+              !((zodRef as z.AnyZodObject).shape[key] instanceof z.ZodNever)
+          ),
         },
         metaSchema
       );
     case zodRef instanceof z.ZodString:
-      return merge({ type: 'string' }, metaSchema);
-    // case zodRef instanceof z.ZodNumber:
-    //   return merge(metaSchema, { type: 'number' });
-    // case zodRef instanceof z.ZodBigInt:
-    //   return merge(metaSchema, { type: 'integer', format: 'int64' });
-    // case zodRef instanceof z.ZodBoolean:
-    //   return merge(metaSchema, { type: 'boolean' });
-    // case zodRef instanceof z.ZodDate:
-    //   return merge(metaSchema, { type: 'string', format: 'date-time' });
-    // case zodRef instanceof z.ZodNull:
-    //   return merge(metaSchema, {
-    //     type: 'string',
-    //     format: 'null',
-    //     nullable: true,
-    //   });
+      return parseZodString(zodRef as z.ZodString, metaSchema);
+    case zodRef instanceof z.ZodNumber:
+      return parseZodNumber(zodRef as z.ZodNumber, metaSchema);
+    case zodRef instanceof z.ZodBigInt:
+      return merge(
+        baseSchema,
+        { type: 'integer', format: 'int64' },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodBoolean:
+      return merge(baseSchema, { type: 'boolean' }, metaSchema);
+    case zodRef instanceof z.ZodDate:
+      return merge(
+        baseSchema,
+        { type: 'string', format: 'date-time' },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodNull:
+      return merge(
+        baseSchema,
+        {
+          type: 'string',
+          format: 'null',
+          nullable: true,
+        },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodOptional:
+    case zodRef instanceof z.ZodNullable:
+      return merge(
+        baseSchema,
+        generateSchema(
+          (
+            zodRef as
+              | z.ZodOptional<OpenApiZodAny>
+              | z.ZodNullable<OpenApiZodAny>
+          ).unwrap()
+        )
+      );
+    case zodRef instanceof z.ZodArray:
+      return merge(
+        baseSchema,
+        {
+          type: 'array',
+          items: generateSchema((zodRef._def as z.ZodArrayDef).type, useOutput),
+        },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodLiteral:
+      return merge(
+        baseSchema,
+        {
+          type: typeof zodRef._def.value as 'string' | 'number' | 'boolean',
+          enum: [zodRef._def.value],
+        },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodEnum:
+    case zodRef instanceof z.ZodNativeEnum:
+      return merge(
+        baseSchema,
+        {
+          type: typeof Object.values(zodRef._def.values)[0] as
+            | 'string'
+            | 'number',
+          enum: Object.values(zodRef._def.values),
+        },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodTransformer:
+    case zodRef instanceof z.ZodEffects:
+      return merge(
+        baseSchema,
+        parseZodTransformation(
+          zodRef as z.ZodEffects<never> | z.ZodTransformer<never>,
+          useOutput
+        ),
+        metaSchema
+      );
+    case zodRef instanceof z.ZodIntersection:
+      return merge(
+        baseSchema,
+        {
+          allOf: [
+            generateSchema(
+              (zodRef as z.ZodIntersection<z.ZodTypeAny, z.ZodTypeAny>)._def
+                .left,
+              useOutput
+            ),
+            generateSchema(
+              (zodRef as z.ZodIntersection<z.ZodTypeAny, z.ZodTypeAny>)._def
+                .right,
+              useOutput
+            ),
+          ],
+        },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodUnion:
+      return merge(
+        baseSchema,
+        {
+          oneOf: (
+            zodRef as z.ZodUnion<[z.ZodTypeAny, ...z.ZodTypeAny[]]>
+          )._def.options.map((schema) => generateSchema(schema, useOutput)),
+        },
+        metaSchema
+      );
+    case zodRef instanceof z.ZodNever:
+      return merge(baseSchema, { readOnly: true }, metaSchema);
+    case zodRef instanceof z.ZodUndefined:
+    case zodRef instanceof z.ZodTuple:
+    case zodRef instanceof z.ZodMap:
+    case zodRef instanceof z.ZodFunction:
+    case zodRef instanceof z.ZodLazy:
+    case zodRef instanceof z.ZodPromise:
+    case zodRef instanceof z.ZodAny:
+    case zodRef instanceof z.ZodUnknown:
+    case zodRef instanceof z.ZodVoid:
     default:
-      return metaSchema;
+      return merge(baseSchema, metaSchema);
   }
 }
-
-// export function zodOpenapi(): string {
-//   return 'zod-openapi';
-// }
-
-// export function convertToSchemaObject(
-//   zodRef: ZodTypeAny,
-//   schemaObject: SchemaObject = {}
-// ): SchemaObject {
-//   if (zodRef.isNullable()) {
-//     schemaObject.nullable = true;
-//   }
-// }
